89cd368b8625a1459a4409e5846b46061e8046cd
PaolaLoera <perl41997@gmail.com>
2024-01-09 15:16:45 -0600

    Terminando ejercios

--- EjerciciosFunciones.js
+++ EjerciciosFunciones.js
@@ -3,10 +3,10 @@
 
 // Función para calcular el perímetro de un círculo
 function perimetroCirculo(radio) {
-  return 2 * 3.1416 * radio;
+  return 2 * 3.14159 * radio;
 }
 
-const perimetroCirculoFlecha = (radio) => 2 * 3.1416 * radio;
+const perimetroCirculoFlecha = (radio) => 2 * 3.14159 * radio;
 
 
 //calcular el area de un rectangulo
@@ -16,9 +16,36 @@ function areaRectangulo(longitud, ancho) {
 }
 const areaRectanguloFlecha = (longitud, ancho) => longitud * ancho;
 
+
 //calcular el cuadrado de un numero
 function cuadradoNumero(numero) {
     const cuadrado = numero * numero;
     console.log("El cuadrado del número " + numero + " es igual a: " + cuadrado);
     return cuadrado;
 }
+const cuadradoNumeroFlecha = (numero) => numero * numero;
+
+
+//calcular la conversion de grados celsius a farenheit
+function celsiusAFahrenheit(celsius) {
+    const fahrenheit = (celsius * 9/5) + 32;
+    console.log(`${celsius} grados Celsius son equivalentes a ${fahrenheit} grados Fahrenheit`);
+    return fahrenheit;
+}
+const celsiusAFahrenheitFlecha = (celsius) => (celsius * 9/5) + 32;
+
+
+//calcular el valor del voltaje dadas la resistencia y la corriente
+function valorVoltaje(corriente, resistencia){
+    const voltaje = corriente * resistencia
+    console.log("El voltaje es: ", voltaje);
+}
+const valorVoltajeFlecha = (corriente, resistencia) => corriente * resistencia;
+
+//calcular el volumen de una esfera
+function volumenEsfera(radioEsfera){
+    const volumen = (4/3) * 3.14159 * (radioEsfera * radioEsfera * radioEsfera);
+    console.log("El volumen de la esfera es:", volumen);
+}
+const volumenEsferaFlecha = (radioEsfera) => (4/3) * 3.14159 * (radioEsfera * radioEsfera * radioEsfera);
+
